5e593c6210839a033b6c3753a73814930c8b1b0e
LeonardRoman <leonardroman1@gmail.com>
2020-03-23 12:05:36 +0400

    Fix. Delete test store

--- src/store/index.js
+++ src/store/index.js
@@ -11,48 +11,9 @@ export default new Vuex.Store({
       { id: 3, name: 'done', title: 'Выполнено', description: '' },
     ],
     tasks: {
-      'work': [{
-        id: 1,
-        name: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt, non?',
-        description: ''
-      },
-        { id: 3, name: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt, non?', description: '' },
-        { id: 4, name: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt, non?', description: '' },
-        { id: 5, name: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt, non?', description: '' },
-        {
-          id: 6,
-          name: 'asdfasdf ;asdjfas audsfyhoiasndfasiodfp ash a  adfasdf adsfas asdf adsfdfasdf asdf ' +
-            'asdf asasdf asdfaf asdfasdfadasdfasf dsfa sdasdf asdasdfdsa asdfasdf asdfsad fasdsdhfa ' +
-            'pahsdfpoasid poasdf pao', description: ''
-        },],
-      'check': [{
-        id: 7,
-        name: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad culpa debitis doloremque dolorum' +
-          ' enim error eveniet fuga incidunt magnam maiores modi nobis, non odit, officia optio possimus ' +
-          'quod reprehenderit temporibus ullam voluptatem! Culpa in iusto minus nesciunt nisi quis, soluta ' +
-          'voluptatem. Architecto eius harum illum iure, minima nisi obcaecati! Ad autem consectetur ducimus ' +
-          'eligendi inventore iusto minus quas reiciendis rerum, sed! Architecto consequuntur ' +
-          'libero temporibus? Aliquid assumenda atque aut autem commodi consequuntur, corporis cumque cupiditate' +
-          ' debitis deleniti dicta dolor dolore eligendi, exercitationem expedita fugiat fugit id illo magnam ' +
-          'minus, nemo odio officia provident quae quibusdam repellat repellendus totam vel veniam?', description: ''
-      },
-        { id: 8, name: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt, non?', description: '' },
-        { id: 9, name: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt, non?', description: '' },
-        { id: 10, name: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt, non?', description: '' },
-        { id: 11, name: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt, non?', description: '' }],
-      'done': [{
-        id: 12,
-        name: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt, non?',
-        description: ''
-      },
-        { id: 13, name: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt, non?', description: '' },
-        {
-          id: 14, name: 'as sdfa dfasdfdfasdf asdfi asidfjasd;fjas jadslfkjasd;j asdfjasdifjasdjf ;klasjdf;l jasdfj' +
-            ' as dfjasdhfasd;fsjadfieaf lkjsdaflj asdoifjsladkj ;alsdfja;sdfjasdfgasudfh; asdfjas;dfijasdoifjasdfn ' +
-            'asdkfjas;dlfjas;dfj', description: ''
-        },
-        { id: 15, name: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt, non?', description: '' },
-        { id: 16, name: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt, non?', description: '' },]
+      'work': [],
+      'check': [],
+      'done': []
     },
   },
   mutations: {
